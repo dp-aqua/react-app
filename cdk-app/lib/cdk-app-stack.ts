84dc45b4c4c442f6c1636bc18929ee9ee6cc01a8
Dev Patel <dev@investwithaqua.com>
2021-10-28 00:03:32 -0400

    Config fixed for docker

--- cdk-app/lib/cdk-app-stack.ts
+++ cdk-app/lib/cdk-app-stack.ts
@@ -269,7 +269,6 @@ export class CdkAppStack extends cdk.Stack {
           },
           build: {
             commands: [
-              "cd flask-docker-app",
               `docker build -t $ECR_REPO_URI:$TAG .`,
               "$(aws ecr get-login --no-include-email)",
               "docker push $ECR_REPO_URI:$TAG",
